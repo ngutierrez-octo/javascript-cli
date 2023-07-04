e5fc32107cf6bcdcabaf7f27646a3fa1de2851ba
ngutierrez-octo <nicolas.a.gutierrez@octo.com>
2023-07-04 15:31:28 +0200

    feat(count_people_animals): add count arg into the app

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const Countries = require("./src/model/countries");
-const data = require("./data").data;
+const Countries = require('./src/model/countries');
+const data = require('./data').data;
 
 const countries = new Countries(data)
 
 const args = process.argv.slice(2);
 const filterArgs = args.filter(arg => arg.startsWith('--filter='));
+const countArg = args.filter(arg => arg.startsWith('--count'));
+
 
 if (filterArgs.length > 0) {
     const filterValue = filterArgs[0].substring('--filter='.length);
     countries.filterAnimals(filterValue)
+    console.dir(countries.toJson(),{depth:null});
+}
+else if (countArg.length >0){
+    countries.countPeoples()
     console.dir(countries.toJson(),{depth:null});
 }
