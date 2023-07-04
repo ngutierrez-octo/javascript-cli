505006a5a41f946f17a7114039e08473753415f5
ngutierrez-octo <nicolas.a.gutierrez@octo.com>
2023-07-04 09:38:05 +0200

    feat(pattern_filtering): init app with filtering option

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const Countries = require("./countries");
+const data = require("./data").data;
+
+const countries = new Countries(data)
+
+const args = process.argv.slice(2);
+const filterArgs = args.filter(arg => arg.startsWith('--filter='));
+
+if (filterArgs.length > 0) {
+    const filterValue = filterArgs[0].substring('--filter='.length);
+    console.dir(countries.filterByAnimals(filterValue),{depth:null});
+
+} else {
+    console.log('No filter value provided');
+}
